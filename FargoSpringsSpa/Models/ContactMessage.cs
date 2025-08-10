2207fd435e58a482fa5d9b79de6458d16580bcfc
reno-ogden <reno.ogden02@gmail.com>
2025-08-10 15:01:42 -0500

    Updated the contact page with a form that adds messages to a database and can be viewed on the Admin dashboard. Also integrated Google Maps link.

---
+++ FargoSpringsSpa/Models/ContactMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FargoSpringsSpa.Models
+{
+    public class ContactMessage
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, EmailAddress, StringLength(120)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(120)]
+        public string Subject { get; set; } = string.Empty;
+
+        [Required, StringLength(2000, MinimumLength = 10)]
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+    }
+}
